cbb60186d168b6ff3820fd60fef1616c907fc95f
holmanjr <holmanjr17@gmail.com>
2017-10-15 13:57:26 -0600

    added to pipe funcionality

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ double timeExecutable(std::string, std::vector<std::string> &, double);
 void runExecutable(std::string, std::vector<std::string> &, double);
 void ptime(double);
 void history(std::vector<std::string> &);
+void pipeFunc(std::vector<std::string>, std::vector<std::string>);
 
 const int PIPE_COUNT = 2;
 const int PIPE_READ_END = 0;
@@ -109,24 +110,43 @@ void runExecutable(std::string args, std::vector<std::string> &hist, double runT
 		return;
 	}
 
+	//User wants to pipe
 	if (std::find(components.begin(), components.end(), "|") != components.end()) {
-		std::cout << "Were in the pipe section" << std::endl;
+		auto left = true;
+		std::vector<std::string> leftSide;
+		std::vector<std::string> rightSide;
+		for (auto &comp : components) {
+			if (comp == "|") {
+				left = false;
+				break;
+			}
+			if (left) {
+				leftSide.push_back(comp);
+			}
+			else {
+				rightSide.push_back(comp);
+			}
+		}
+		
+		pipeFunc(leftSide, rightSide);
 	}
 
-	char** argv;
-	argv = new char*[n];
+	else {
+		char** argv;
+		argv = new char*[n];
 
-	//Unload arguments from vector into correct data types
-	for (int i = 0; i<n; i++) {
-		argv[i] = new char[components[i].size() + 1];
-		strcpy(argv[i], components[i].c_str());
-	}
-	argv[n] = NULL;
+		//Unload arguments from vector into correct data types
+		for (int i = 0; i < n; i++) {
+			argv[i] = new char[components[i].size() + 1];
+			strcpy(argv[i], components[i].c_str());
+		}
+		argv[n] = NULL;
 
-	//Run execution inside if statement
-	if (execvp(argv[0], argv)<0) {//Execution failed
-		std::cout << "ERROR: exec failed" << std::endl;
-		exit(1);
+		//Run execution inside if statement
+		if (execvp(argv[0], argv) < 0) {//Execution failed
+			std::cout << "ERROR: exec failed" << std::endl;
+			exit(1);
+		}
 	}
 }
 
@@ -142,4 +162,10 @@ void history(std::vector<std::string> &hist) {
 		std::cout << i + 1 << " : " << hist[i] << std::endl;
 	}
 	std::cout << std::endl;
+}
+
+void pipeFunc(std::vector<std::string> lSide, std::vector<std::string> rSide) {
+	std::cout << "Pipe function:" << std::endl;
+	std::cout << "Left side: " << lSide[0] << std::endl;
+	std::cout << "Right side: " << rSide[0] << std::endl;
 }
